e338f13320d6c2b58030a4232e44dc7f0f99636a
otuyamicah <otuyamicah@gmail.com>
2021-09-17 00:42:45 +0100

    js_objects

---
+++ js_objects_exercises/objects_exercise.js
@@ -0,0 +1,51 @@
+// An object literal
+var car = {
+    name: 'Chevrolet',
+    model: 'Camaro ZL1',
+    year: 2021,
+    color: 'white & black',
+    ownerName: 'BossMicah',
+    ownerNumber: '08169371046',
+    mileage:  '10 miles',
+
+}
+console.log(car);
+// mutating a value in an object
+// car.name = "Chevrolet"   
+
+// adding a newkey value in an object
+// car["shape"] = "plane"
+
+// deletting a value in an object
+// delete car["year"]
+
+
+
+
+
+console.log("")
+
+
+
+// An object using a newKeyword
+
+var car = new Object();
+
+
+car.name = 'Chevrolet';
+car.model = 'Camaro ZL1';
+car.year = 2021;
+car.color ='white & black';
+car.ownerName = 'BossMicah';
+car.ownerNumber = '08169371046';
+car.mileage =  '10 miles';
+
+console.log(car.color)
+console.log(car.year)
+console.log(car.model)
+console.log(car.name)
+
+
+
+
+
