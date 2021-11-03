11f1dfea6861410db4cc22bda9c4745589e88575
otuyamicah <otuyamicah@gmail.com>
2021-11-03 11:58:45 +0100

    functions

---
+++ execution_stack.js
@@ -0,0 +1,19 @@
+function one(){
+    return 'i am function one'
+}
+console.log(one())
+
+
+function two(){
+    return 'i am function two'
+   
+}
+console.log(two())
+
+
+
+function three(){
+    return 'i am function three'
+    
+}
+console.log(three())
